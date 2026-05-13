9bc3210f6ce5771c47394527a24ec75da7762ff6
Renth <135339655+Arch-Ren@users.noreply.github.com>
2026-05-13 14:09:05 +0700

    Database testing

--- src/main/java/task_tracker/DemoApplication.java
+++ src/main/java/task_tracker/DemoApplication.java
@@ -2,6 +2,12 @@ package task_tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import task_tracker.entity.User;
+import task_tracker.entity.Tasks;
+import task_tracker.repository.UserRepository;
+import task_tracker.repository.TasksRepository;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +16,24 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserRepository userRepo, TasksRepository taskRepo) {
+		return args -> {
+			//Buat User Baru
+			User user = new User();
+			user.setUsername("Renth Demo");
+			user.setEmail("renth@gmail.com");
+			userRepo.save(user);
+
+			//buat tasks untuk user
+			Tasks task = new Tasks();
+			task.setDescription("Projek mandiri Task Tracker");
+			task.setStatus("IN_PROGRESS");
+			task.setUser(user);
+			taskRepo.save(task);
+
+			//tester
+			System.out.println("Data berhasil disimpan ke database");
+		};
+	}
 }
