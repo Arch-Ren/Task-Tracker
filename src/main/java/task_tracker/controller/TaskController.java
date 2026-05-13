2468a26b4b97a8febd31a362623fb2d844d0da9a
Renth <135339655+Arch-Ren@users.noreply.github.com>
2026-05-13 14:30:39 +0700

    feat: Read all tasks

---
+++ src/main/java/task_tracker/controller/TaskController.java
@@ -0,0 +1,23 @@
+package task_tracker.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import task_tracker.entity.Tasks;
+import task_tracker.repository.TasksRepository;
+
+import java.util.List;
+
+@RestController     //penanda class yang menangani API
+@RequestMapping("/api/tasks")       //alamat URL
+public class TaskController {
+    
+    @Autowired
+    private TasksRepository taskRepository;
+
+    @GetMapping
+    public List<Tasks> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
